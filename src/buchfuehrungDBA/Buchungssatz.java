94d0b3fd2ceffdf28831abe690ea9dcefe8dfbb3
LetitiaJD <letitiajdawson@gmail.com>
2020-06-19 13:22:31 +0200

    Removed methods buchungDurchfuehren() and bucheSollUndHaben() to repair merge conflict

--- src/buchfuehrungDBA/Buchungssatz.java
+++ src/buchfuehrungDBA/Buchungssatz.java
@@ -14,43 +14,6 @@ public class Buchungssatz {
         this.buchungsbetrag = buchungsbetrag;
     }
 
-    public String buchungDurchfuehren()
-    {
-        switch(this.sollKonto.getClass().getName()) {
-            case "Passivkonto":
-            if ((sollKonto.getSummeHabenBuchungen() - sollKonto.getSummeSollBuchungen()) < this.buchungsbetrag) {
-                return "Die Buchung konnte nicht durchgeführt werden, weil nicht genug Geld auf dem Konto ist.";
-            }
-            break;
-            case "Ertragskonto":
-                return "Die Buchung konnte nicht durchgeführt werden, weil ...";
-            default:
-            break;
-        }
-
-        switch(this.habenKonto.getClass().getName()) {
-            case "Aktivkonto":
-            if ((habenKonto.getSummeSollBuchungen() - habenKonto.getSummeHabenBuchungen()) < this.buchungsbetrag) {
-                return "Die Buchung konnte nicht durchgeführt werden, weil nicht genug Geld auf dem Konto ist.";
-            }
-            break;
-            case "Aufwandskonto":
-                return "Die Buchung konnte nicht durchgeführt werden, weil ...";
-            default:
-            break;
-        }
-
-        return this.bucheSollUndHaben(buchungsbetrag);
-    }
-
-    public String bucheSollUndHaben(double buchungsbetrag)
-    {
-        this.habenKonto.bucheHaben(buchungsbetrag);
-        this.sollKonto.bucheSoll(buchungsbetrag);
-        
-        return "Die Buchung wurde ordnungsgemäß durchgeführt.";
-    }
-
     public String ausgeben()
     {
         String ausgabe = new String();
